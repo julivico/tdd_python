563a4e5508ddfb18079e4c7eb64a60413798a293
Quoc Viet Pham <julivico3979@hotmail.com>
2015-10-16 10:46:21 +0200

    split unit_test

---
+++ lists/tests/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+__init__.py
+
+
+"""
+
+__author__ = 'viet.pham'
+__copyright__ = 'GiQ GmbH'
+__date__ = '16.10.2015'
+__version__ = '0.0.1'
+__status__ = 'beta'
